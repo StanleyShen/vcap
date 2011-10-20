ace097bbb4232591a2c734d87dfffd8813f7ec98
hmalphettes <hmalphettes@intalio.com>
2011-10-20 15:07:24 +0800

    Finsih the naming conventuion refactoring for the postgres node recipe

--- dev_setup/cookbooks/postgresql/recipes/default.rb
+++ dev_setup/cookbooks/postgresql/recipes/default.rb
@@ -49,3 +49,7 @@ EOH
 else
   Chef::Log.error("Installation of PostgreSQL is not supported on this platform.")
 end
+
+cf_pg_update_hba_conf(node[:postgresql][:database], node[:postgresql][:server_root_user])
+cf_pg_setup_db(node[:postgresql][:database], node[:postgresql][:server_root_user], node[:postgresql][:server_root_password])
+
