5aaea0784ae10c8b25b7000ca700e382c625c558
hmalphettes <hmalphettes@intalio.com>
2011-10-20 13:10:04 +0800

    Make the cloud_controller more configurable in its chef recipe.

--- dev_setup/cookbooks/cloud_controller/attributes/default.rb
+++ dev_setup/cookbooks/cloud_controller/attributes/default.rb
@@ -4,6 +4,10 @@ default[:cloud_controller][:service_api_uri] = "http://api.#{node[:deployment][:
 default[:cloud_controller][:local_route] = nil
 default[:cloud_controller][:admins] = ["dev@cloudfoundry.org"]
 
+# Specifies if new users can register only from the host that is running the cloud controller
+default[:cloud_controller][:local_register_only] = false
+default[:cloud_controller][:app_uris][:allow_external] = false
+
 # Staging
 default[:cloud_controller][:staging][:grails] = "grails.yml"
 default[:cloud_controller][:staging][:lift] = "lift.yml"
@@ -14,6 +18,18 @@ default[:cloud_controller][:staging][:rails3] = "rails3.yml"
 default[:cloud_controller][:staging][:sinatra] = "sinatra.yml"
 default[:cloud_controller][:staging][:spring] = "spring.yml"
 default[:cloud_controller][:staging][:java_web] = "java_web.yml"
+
+default[:cloud_controller][:staging][:max_staging_runtime] = 120 #seconds
+default[:cloud_controller][:staging][:secure] = false
+
+# Default account capacity
+default[:cloud_controller][:default_account_capacity][:memory ] = 2048
+default[:cloud_controller][:default_account_capacity][:app_uris ] = 8
+default[:cloud_controller][:default_account_capacity][:services ] = 4
+default[:cloud_controller][:default_account_capacity][:apps ] = 4
+
+# Enable/disable starting apps in debug modes.
+default[:cloud_controller][:allow_debug] = true
 
 # Default builtin services
 default[:cloud_controller][:builtin_services] = ["redis", "mongodb", "mysql", "neo4j", "postgresql"]
