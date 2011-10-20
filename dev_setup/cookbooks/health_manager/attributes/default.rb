22a7dd0500b5b17df8b53f07074b197b7319edce
hmalphettes <hmalphettes@intalio.com>
2011-10-20 16:57:16 +0800

    Complete the refactoring of the postgres keys to have a suffix _node

--- dev_setup/cookbooks/health_manager/attributes/default.rb
+++ dev_setup/cookbooks/health_manager/attributes/default.rb
@@ -1,4 +1,4 @@
-include_attribute "postgresql_node"
+include_attribute "postgresql"
 default[:health_manager][:config_file] = "health_manager.yml"
 default[:health_manager][:database][:username] = node[:postgresql_node][:server_root_user]
 default[:health_manager][:database][:password] = node[:postgresql_node][:server_root_password]
