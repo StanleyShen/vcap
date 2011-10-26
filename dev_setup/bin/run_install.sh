53a6d408cb900f9dfa96fa04ac14834614689581
hmalphettes <hmalphettes@intalio.com>
2011-10-26 19:46:52 +0800

    use the same json_pure than vcap

--- dev_setup/bin/run_install.sh
+++ dev_setup/bin/run_install.sh
@@ -25,7 +25,7 @@ if [ $? == 0 ]; then
   sudo gem install mime-types
   sudo gem install rubyzip2
   sudo gem install terminal-table
-  sudo gem install json_pure --version "1.5.1"
+  sudo gem install json_pure --version "1.5.4"
   sudo gem install vmc --version "0.3.13.beta.4" #or later (or the stable one)
 fi
 which vmc > /dev/null
