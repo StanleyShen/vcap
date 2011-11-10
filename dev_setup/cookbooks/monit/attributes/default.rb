a8b2bcdf515207c1958669aa8a821d9459a13889
hmalphettes <hmalphettes@intalio.com>
2011-11-10 14:49:55 +0800

    debugging some details for monit

--- dev_setup/cookbooks/monit/attributes/default.rb
+++ dev_setup/cookbooks/monit/attributes/default.rb
@@ -1,3 +1,3 @@
-node[:monit][:config_file] = File.join(node[:deployment][:config_path], "vcap.monitrc")
-node[:monit][:vcap_exec] = node[:deployment][:vcap_exec]
-node[:monit][:daemon_startup] = -1 #0, will disable starting as a daemon, 1 will enable it; anything else leaves it unchanged
+default[:monit][:config_file] = File.join(node[:deployment][:config_path], "vcap.monitrc")
+default[:monit][:vcap_exec] = node[:deployment][:vcap_exec]
+default[:monit][:daemon_startup] = -1 #0, will disable starting as a daemon, 1 will enable it; anything else leaves it unchanged
