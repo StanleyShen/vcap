4fbcd14e28b0c2279f1019140ae6b58eb1cc22d5
hmalphettes <hmalphettes@intalio.com>
2011-12-05 15:39:22 +0800

    vcap: don't start/stop nats-server when it is managed as an OS SysV daemon. nats-server: Improved nats-server start/stop: detect an already running nats-server reliably.

--- dev_setup/cookbooks/nats_server/attributes/default.rb
+++ dev_setup/cookbooks/nats_server/attributes/default.rb
@@ -5,4 +5,5 @@ default[:nats_server][:password] = "nats"
 default[:nats_server][:logtime] = "true"
 default[:nats_server][:debug] = "false"
 default[:nats_server][:trace] = "false"
+default[:nats_server][:is_os_daemon] = true
 #default[:nats_server][:tracked_net_iface] = "eth0"
