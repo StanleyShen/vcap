56030239ce0afbab29920ddc6dfdc8b29ba09bbe
hmalphettes <hmalphettes@intalio.com>
2012-01-17 13:22:43 +0800

    Further commits to make the recipes catch up with the latest changes.

--- dev_setup/cookbooks/cloud_controller/attributes/default.rb
+++ dev_setup/cookbooks/cloud_controller/attributes/default.rb
@@ -9,7 +9,7 @@ default[:cloud_controller][:description] = "VMware's Cloud Application Platform"
 default[:cloud_controller][:support_address] = "http://support.cloudfoundry.com"
 
 # Specifies if new users can register only from the host that is running the cloud controller
-default[:cloud_controller][:local_register_only] = false
+default[:cloud_controller][:allow_registration] = true
 default[:cloud_controller][:app_uris][:allow_external] = false
 
 # Staging
@@ -25,9 +25,18 @@ default[:cloud_controller][:staging][:java_web] = "java_web.yml"
 default[:cloud_controller][:staging][:java_start] = "java_start.yml"
 default[:cloud_controller][:staging][:php] = "php.yml"
 
+# keys
+default[:cloud_controller][:keys][:password] = 'password key goes here'
+default[:cloud_controller][:keys][:token] = 'token key goes here'
+default[:cloud_controller][:keys][:token_expiration] = 604800 # 7 * 24 * 60 * 60 (= 1 week)
+
 #Stager
 default[:cloud_controller][:stager][:max_staging_runtime] = 120 #seconds
 default[:cloud_controller][:stager][:secure] = false
+default[:cloud_controller][:stager][:new_stager_percent] = 0
+default[:cloud_controller][:stager][:new_stager_email_regexp] = '@@' #won't match any email.
+#default[:cloud_controller][:stager][:auth][:user] = vcap
+#default[:cloud_controller][:stager][:auth][:password] = vcap
 
 # Enable/disable starting apps in debug modes.
 default[:cloud_controller][:allow_debug] = true
