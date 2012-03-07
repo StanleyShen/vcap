c0b362e871a4ca33a9d2ad5d0903fa080c4d5661
hmalphettes <hmalphettes@intalio.com>
2012-03-07 16:55:37 +0800

    trivial mistake in the mongodb recipe that was skipping the actual installation of the mongod binaries

--- dev_setup/cookbooks/mongodb/recipes/default.rb
+++ dev_setup/cookbooks/mongodb/recipes/default.rb
@@ -7,10 +7,9 @@
 #
 compute_derived_attributes
 
-mongod_bin_path=node[:mongodb_node][:path]
+mongod_bin_path=File.join(node[:mongodb_node][:path], 'bin','mongod')
 expected_version=node[:mongodb_node][:version]
 expected_version_found=`echo $(#{mongod_bin_path} --version 2>&1) | grep v#{expected_version}` if File.exists?(mongod_bin_path)
-
 
 remote_file File.join("", "tmp", "mongodb-linux-#{node[:kernel][:machine]}-#{node[:mongodb_node][:version]}.tgz") do
   owner node[:deployment][:user]
@@ -34,6 +33,12 @@ cd mongodb-linux-#{node[:kernel][:machine]}-#{node[:mongodb_node][:version]}
 [ -d #{node[:mongodb_node][:path]} ] && rm -rf #{node[:mongodb_node][:path]}/*
 mkdir -p #{node[:mongodb_node][:path]}/bin
 cp #{File.join("bin", "*")} #{File.join(node[:mongodb_node][:path], "bin")}
+# sanity check:
+if [ ! -f #{mongod_bin_path} ]; then
+  echo "Installation of mongod failed: #{mongod_bin_path} does not exist"
+  exit 1
+fi
+echo $(#{mongod_bin_path} --version 2>&1) | grep v#{expected_version}
 EOH
   not_if do
     expected_version_found
