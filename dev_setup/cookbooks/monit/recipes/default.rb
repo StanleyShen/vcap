c1b24dcb546bdf73585c897d1316cb28449a8b5f
hmalphettes <hmalphettes@intalio.com>
2012-03-09 17:39:26 +0800

    Make the monit recipe idempotent, and take care of restarting monit if something changes.

--- dev_setup/cookbooks/monit/recipes/default.rb
+++ dev_setup/cookbooks/monit/recipes/default.rb
@@ -6,6 +6,20 @@ compute_derived_attributes
 
 package "monit"
 
+# this will run /etc/init.d/monit start
+service "monit" do
+  # does not fail if the service was not running
+  restart_command "/etc/init.d/monit stop; /etc/init.d/monit start"
+  action :nothing
+end
+
+# this will fail and prevent us from restarting nagios if we broke the config
+execute "test-monit-config" do
+  command "/etc/init.d/monit syntax"
+  notifies :restart, "service[monit]"
+  action :nothing
+end
+
 node[:monit][:config_file] = File.join(node[:deployment][:config_path], "vcap.monitrc")
 
 case node['platform']
@@ -35,12 +49,13 @@ if [ ! -d monit-5.3 ]; then
   # set httpd port 2812 and
   #     use address localhost  # only accept connection from localhost
   #     allow localhost        # allow localhost to connect to the server and
+  # this is set by default in 12.04
   sudo sed -i 's/^#[[:space:]]*set httpd port 2812 and/set httpd port 2812 and/g' /etc/monit/monitrc
   sudo sed -i 's/^#[[:space:]]*use address localhost/    use address localhost/g' /etc/monit/monitrc
   sudo sed -i 's/^#[[:space:]]*allow localhost/    allow localhost/g' /etc/monit/monitrc
-  sudo monit
 fi
 EOH
+      notifies :run, "execute[test-monit-config]", :immediately
       not_if do
         # only 10.04 has a really old version of monit that needs an upgrade
         node[:platform_version].to_f >= 11.10
@@ -48,40 +63,83 @@ EOH
     end
     
     #Startup mode
-    if node[:monit][:daemon_startup] == 1 || node[:monit][:daemon_startup] == 0
-      bash "Setup monit daemon startup mode to #{node[:monit][:daemon_startup]}" do
-        user "root"
-        code <<-EOH
-sed -i 's/^startup=.*$/startup=#{node[:monit][:daemon_startup]}/g' /etc/default/monit
-sed -i 's/^#.*set daemon  120/set daemon  120/g' /etc/monit/monitrc
-EOH
+    template "/etc/network/if-up.d/monit_dameon" do
+      path "/etc/network/if-up.d/monit_dameon"
+      source "monit_dameon_if_up.erb"
+      mode 0755
+      action :create
+      only_if do
+        node[:monit][:network_startup] == 1 || node[:monit][:network_startup] == true
       end
     end
-    if node[:monit][:network_startup] == 1 || node[:monit][:network_startup] == true
-      template "/etc/network/if-up.d/monit_dameon" do
-        path "/etc/network/if-up.d/monit_dameon"
-        source "monit_dameon_if_up.erb"
-        mode 0755
-      end
-
-    end
-    
-    #Include directive.
+        
+###found=`egrep "^[[:space:]]*include \/etc\/monit\/config\.d\/\*" /etc/monit/monitrc`
+    #Include directive and Startup mode
     bash "Setup monit daemon startup mode to #{node[:monit][:daemon_startup]}" do
       user "root"
       code <<-EOH
-    found=`egrep ^include\ \/etc\/monit\/conf\.d\/\* /etc/monit/monitrc`
-    [ -z "$found" ] && echo "include /etc/monit/conf.d/*" >> /etc/monit/monitrc
-    echo "include #{node[:monit][:config_file]}" > /etc/monit/conf.d/include_vcap.monitrc
+sed -i 's/^startup=.*$/startup=#{node[:monit][:daemon_startup]}/' /etc/default/monit
+found=`egrep "^[[:space:]]*include /etc/monit/config\.d/\*" /etc/monit/monitrc`
+[ -z "$found" ] && echo "include /etc/monit/config.d/*" >> /etc/monit/monitrc
+echo "include #{node[:monit][:config_file]}" > /etc/monit/config.d/include_vcap.monitrc
+
+found=`egrep "^[[:space:]]*with start delay " /etc/monit/monitrc`
+echo "Found start delay: $found"
+if [ -z "$found" ]; then
+  found_commented=`egrep "^#[[:space:]]*with start delay " /etc/monit/monitrc`
+  if [ -z "$found_commented" ]; then
+    echo "Could not find the commented line for the delay '#   with start delay 240' in /etc/monit/monitrc: not supported"
+    exit 1
+  else
+    sed -i 's/^#[[:space:]]*with start delay .*/   with start delay 240 # delay the start by 4 minutes/' /etc/monit/monitrc
+  fi
+fi
 EOH
+      notifies :run, "execute[test-monit-config]", :immediately
+      only_if do
+        def monit_already_configured()
+          Chef::Log.debug("::File.exists?(/etc/monit/monitrc) #{::File.exists?('/etc/monit/monitrc')}")
+          Chef::Log.debug("::File.exists?(/etc/default/monit) #{::File.exists?('/etc/default/monit')}")
+          return true unless ::File.exists?("/etc/monit/monitrc")
+          return true unless ::File.exists?("/etc/default/monit")
+          start_delay = nil
+          include_config_d = nil
+          ::File.readlines("/etc/monit/monitrc").collect do |name|
+            start_delay ||= name[/^[[:space:]]*with start delay/]
+            include_config_d ||= name[/^[[:space:]]*include \/etc\/monit\/config\.d\/\*/]
+          end
+          Chef::Log.debug("start_delay #{start_delay}")
+          Chef::Log.debug("include_config_d #{include_config_d}")
+          return true if start_delay.nil? || include_config_d.nil?
+          startup_yes = nil
+          ::File.readlines("/etc/default/monit").collect do |name|
+            startup_yes ||= name == "startup=#{node[:monit][:daemon_startup]}"
+          end
+          Chef::Log.debug("startup_yes #{startup_yes}")
+          return true if startup_yes.nil?
+          Chef::Log.info("monit is already configured to start as a daemon with a delay")
+          return false
+        end
+        monit_already_configured()
+      end
+    end
+    
+    directory "/etc/monit/config.d" do
+      mode "0755"
+      recursive true
+      action :create
     end
     
     #postgres?
-    if !node[:monit][:others].nil? && node[:monit][:others].include?("postgresql")
-      template "/etc/monit/conf.d/postgresql.monitrc" do
-        path "/etc/monit/conf.d/postgresql.monitrc"
-        source "postgresql.monitrc.erb"
-        mode 0644
+    
+    template "/etc/monit/config.d/postgresql.monitrc" do
+      path "/etc/monit/config.d/postgresql.monitrc"
+      source "postgresql.monitrc.erb"
+      mode 0644
+      action :create
+      notifies :run, "execute[test-monit-config]", :immediately
+      only_if do
+        !node[:monit][:others].nil? && node[:monit][:others].include?("postgresql")
       end
     end
     
@@ -106,7 +164,8 @@ if node[:monit][:vcap_components].include?("dea")
     |name| name =~ /_node$/ || name =~ /^router$/ || name =~ /^cloud_controller$/
   }
   unless nodes_components.empty?
-    dea_deps = node[:monit][:depends_on]["dea"].nil? ? nodes_components : ( node[:monit][:depends_on]["dea"] + nodes_components )
+    dea_deps = node[:monit][:depends_on]["dea"].nil? ?
+      nodes_components : ( node[:monit][:depends_on]["dea"] + nodes_components )
     node[:monit][:depends_on]["dea"] = dea_deps
   end
 end
@@ -114,6 +173,8 @@ end
 template node[:monit][:config_file] do
   path node[:monit][:config_file]
   source "vcap.monitrc.erb"
+  action :create
+  notifies :run, "execute[test-monit-config]", :immediately
   owner node[:deployment][:user]
   mode 0644
 end
