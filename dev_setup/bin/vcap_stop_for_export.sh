cde070ac25630cf716328cd62c8335d04f745aae
Andy <zhou@intalio.com>
2012-12-24 12:21:36 +0800

    fix grub path

--- dev_setup/bin/vcap_stop_for_export.sh
+++ dev_setup/bin/vcap_stop_for_export.sh
@@ -206,7 +206,7 @@ sudo apt-get remove mysql-common -y
 sudo apt-get remove subversion -y
 
 # disable grub menu
-sudo sed -i  -e 's/#\(GRUB_HIDDEN_TIMEOUT=\)/\1/' -e 's/#\(GRUB_HIDDEN_TIMEOUT_QUIET=\)/\1/' grub
+sudo sed -i  -e 's/#\(GRUB_HIDDEN_TIMEOUT=\)/\1/' -e 's/#\(GRUB_HIDDEN_TIMEOUT_QUIET=\)/\1/' /etc/default/grub
 sudo update-grub2
 # remove previous linux image
 dpkg -l 'linux-*' | sed '/^ii/!d;/'"$(uname -r | sed "s/\(.*\)-\([^0-9]\+\)/\1/")"'/d;s/^[^ ]* [^ ]* \([^ ]*\).*/\1/;/[0-9]/!d' | xargs sudo apt-get -y purge
