5988bf8bc14a10ecc058b6febb3877dcb20af55f
arkxu <fangzhou@arkxu.com>
2014-05-23 16:00:40 -0700

    update ruby version

--- dev_setup/cookbooks/ruby/attributes/default.rb
+++ dev_setup/cookbooks/ruby/attributes/default.rb
@@ -1,6 +1,6 @@
 include_attribute "deployment"
 
-default[:ruby][:version] = "1.9.3-p545"
+default[:ruby][:version] = "1.9.3-p547"
 default[:ruby][:version_regexp] = Regexp.quote(ruby[:version]).gsub(/-/, '-?')
 default[:ruby][:source]  = "http://ftp.ruby-lang.org//pub/ruby/1.9/ruby-#{ruby[:version]}.tar.gz"
 default[:ruby][:path]    = File.join(node[:deployment][:home], "deploy", "rubies", "ruby-#{ruby[:version]}")
