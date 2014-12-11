b451833e235dbed95139076a72dceb13d0906697
Andy <zhou@intalio.com>
2014-12-11 12:41:48 +0800

    change the logic of can_upgrade

--- create_admin/lib/jobs/apps_version.rb
+++ create_admin/lib/jobs/apps_version.rb
@@ -22,7 +22,7 @@ class ::Jobs::AppsVersion
         if (v['version'] == 'NOT_AVAILABLE')
           can_upgrade = true
         else
-          can_upgrade = ava_version['version'] > v['version']
+          can_upgrade = can_upgrade?(v['version'], ava_version['version'])
         end
       end
 
@@ -30,5 +30,31 @@ class ::Jobs::AppsVersion
     }
     
     completed({'versions' => res})
+  end
+
+  # upgrade logic:
+  # 1) current version has same major number as available version 
+  # and
+  # 2) available version has bigger minor number than current version 
+  # 2b) if minor numbers are then same, available version has bigger patch number than current version
+  # 2c) if minor numbers and patch numbers are the same, available version has bigger build number than current version
+  def can_upgrade?(cur_ver, ava_ver)
+    cur_nums = cur_ver.split('.')
+    ava_nums = ava_ver.split('.')
+
+    return false if cur_nums[0] != ava_nums[0]
+
+    # version format is: Major.minor.patch.build
+    return true if (ava_nums[1].to_i > cur_nums[1].to_i)
+
+    if (ava_nums[1].to_i == cur_nums[1].to_i)
+      return true if (ava_nums[2].to_i > cur_nums[2].to_i)
+
+      if (ava_nums[2].to_i == cur_nums[2].to_i)
+        return true if (ava_nums[3].to_i > cur_nums[3].to_i)
+      end
+    end
+
+    false
   end
 end
