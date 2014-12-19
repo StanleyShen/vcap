0656f7fa0b6a2392dcb858a21a96bd8134127888
arkxu <fangzhou@arkxu.com>
2014-12-19 03:05:09 -0800

    move nfs to vcap

---
+++ dev_setup/cookbooks/nfs/metadata.rb
@@ -0,0 +1,7 @@
+maintainer       "Intalio Inc"
+maintainer_email "eng-support@intalio.com"
+license          "Apache 2.0"
+description      "Installs/Configures network file system"
+version          "0.1"
+depends "deployment"
+
