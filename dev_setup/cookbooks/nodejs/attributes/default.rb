f40e2006529b126109ad125662085fc7c1b6f9b5
sam <sam@intalio.com>
2015-06-09 13:35:29 +0800

    FT-INTALIO-000093 Version upgrades for M25 Upgrade nodejs from 0.10.28 to 0.12.4

--- dev_setup/cookbooks/nodejs/attributes/default.rb
+++ dev_setup/cookbooks/nodejs/attributes/default.rb
@@ -1,4 +1,4 @@
 include_attribute "deployment"
-default[:nodejs][:version] = "0.10.28"
+default[:nodejs][:version] = "0.12.4"
 default[:nodejs][:path] = File.join(node[:deployment][:home], "deploy", "nodejs")
 default[:nodejs][:source] = "http://nodejs.org/dist/v#{node[:nodejs][:version]}/node-v#{node[:nodejs][:version]}-linux-x64.tar.gz"
