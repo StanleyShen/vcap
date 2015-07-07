8941fc56521eedaf6e6de298fb71273b959aea0d
Andy <zhou@intalio.com>
2015-07-07 12:46:19 +0800

    install scrypt into global path

--- dev_setup/cookbooks/nodejs/attributes/default.rb
+++ dev_setup/cookbooks/nodejs/attributes/default.rb
@@ -2,3 +2,5 @@ include_attribute "deployment"
 default[:nodejs][:version] = "0.12.4"
 default[:nodejs][:path] = File.join(node[:deployment][:home], "deploy", "nodejs")
 default[:nodejs][:source] = "http://nodejs.org/dist/v#{node[:nodejs][:version]}/node-v#{node[:nodejs][:version]}-linux-x64.tar.gz"
+
+default[:scrypt][:version] = "4.0.7"
